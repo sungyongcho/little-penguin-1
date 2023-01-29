873c70a9c1db2342cb1210e43b76ca18d3ca17e8
Sungyong Cho <to.sungyongcho@gmail.com>
2023-01-29 00:15:00 +0000

    done; assignment related to USB needs to be checked

--- assignment-09/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/sucho/little-penguin-1/assignment-09/modules.order := {   echo /home/sucho/little-penguin-1/assignment-09/main.o; :; } > /home/sucho/little-penguin-1/assignment-09/modules.order
